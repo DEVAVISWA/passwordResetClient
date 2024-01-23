1b8231bf210b09736bb8e63b6478c0becbaffd6d
DEVAVISWA G <devaviswa15@gmail.com>
2024-01-24 00:56:51 +0530

    Error doubt in letterCamelCasing

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,26 @@
-import axios from 'axios'
 import React, { useState } from 'react'
 
 function Login() {
     const [loginForm, setLoginform] = useState({
-        Email: '',
-        Password: ''
+        email: '',
+        password: ''
     })
     const loginFormHandler = async (e) => {
         e.preventDefault()
         // console.log(loginForm)
-        const loginBody ={
-            Email:loginForm.Email,
-            Password:loginForm.Password
-        }
-        try{
-            const response = await axios.post('http://127.0.0.1:3001/login', loginBody)
-            console.log(response.data)
-        } catch(e) {
-            console.log('err logging in user', e)
+        const response= await fetch('http://127.0.0.1:3001/login',{
+            method: 'POST',
+            headers:{
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(loginForm)
+        })
+        const data = await response.json()
+        if(response.status==200){
+            console.log('login successfull')
+            console.log(data)
+        }else{
+            console.log('err logging in')
         }
     }
     return (
@@ -27,13 +30,13 @@ function Login() {
                 <b>Email</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <input
                     type="email"
-                    value={loginForm.Email}
-                    onChange={(e) => setLoginform({ ...loginForm, Email: e.target.value })} />  <br /> <br />
+                    value={loginForm.email}
+                    onChange={(e) => setLoginform({ ...loginForm, email: e.target.value })} />  <br /> <br />
                 <b>Password</b>&nbsp;&nbsp;
                 <input
                     type="password"
-                    value={loginForm.Password}
-                    onChange={(e) => setLoginform({ ...loginForm, Password: e.target.value })} /> <br /> <br />
+                    value={loginForm.password}
+                    onChange={(e) => setLoginform({ ...loginForm, password: e.target.value })} /> <br /> <br />
                 <button type='submit'>Login</button> &nbsp; &nbsp; &nbsp;
             </form> <br />
             <button type='submit'>Forgot Password?</button>
